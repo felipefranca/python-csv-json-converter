054bb474eec5783f9e846f67dc5cd0e4ea43486d
Felipe Franca <felipejonataoliveira@gmail.com>
2021-10-20 09:04:55 -0300

    Creating converter initial executor to bash

---
+++ python_csv_json_converter/converter.py
@@ -0,0 +1,12 @@
+import logging
+
+from pathlib import Path
+
+import click
+
+import pandas as pd
+
+logging.basicConfig(
+  level="DEBUG", format="'%(asctime)s - %(name)s - %(levelname)s - %(message)s'"
+)
+
